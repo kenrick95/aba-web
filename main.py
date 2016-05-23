1537f86f68e2ac4711805e4f527f5c47c40b2c93
kenrick95 <kenrick95@gmail.com>
2016-05-23 10:36:06 +0800

    simple request + response, basic visualization

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import tornado.ioloop
 import tornado.web
-
+from aba.aba_parser import ABA_Parser
+import networkx as nx
+from networkx.readwrite import json_graph
+import json
 
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         self.render("views/index.html")
 
+
+class ApiHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        source_code = self.get_argument('source_code')
+        parser = ABA_Parser(source_code)
+        parse_result = parser.parse()
+        aba = parser.construct_aba()
+        
+        graph = aba.arguments[0].graph
+        data = json_graph.node_link_data(graph)
+        
+        self.write(json.dumps(data))
+        self.flush()
+        self.finish()
+
 def make_app():
     return tornado.web.Application([
         (r"/", IndexHandler),
+        (r"/api/?", ApiHandler),
     ])
 
 if __name__ == "__main__":
