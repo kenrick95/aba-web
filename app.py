652e74c1d82f3ab65a6ad37fa9d13f9ca0216d2b
kenrick95 <kenrick95@gmail.com>
2016-05-23 13:35:57 +0800

    update config for azure deployment

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ import main
 
 application = main.make_app()
 wsgi_app = tornado.wsgi.WSGIAdapter(application)
-server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
-server.serve_forever()
+if __name__ == '__main__':
+    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    server.serve_forever()
