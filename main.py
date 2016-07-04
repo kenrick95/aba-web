803144a815468d643b74ccedced0a85bd918f958
kenrick95 <kenrick95@gmail.com>
2016-07-04 20:48:11 +0800

    more checking if dispute tree exists

--- main.py
+++ main.py
@@ -31,11 +31,13 @@ def api():
     data['dispute_trees_data'] = dict()
 
     for symbol in aba.nonassumptions:
-       data['dispute_trees'][symbol] = jsonpickle.encode(json_graph.node_link_data(aba.get_dispute_tree(symbol).graph), unpicklable = False, max_depth = 6, make_refs = False)
-       data['dispute_trees_data'][symbol] = dict()
-       data['dispute_trees_data'][symbol]['is_conflict_free'] = aba.get_argument(symbol).is_conflict_free()
-       data['dispute_trees_data'][symbol]['is_admissible'] = aba.get_dispute_tree(symbol).is_admissible
-       data['dispute_trees_data'][symbol]['is_grounded'] = aba.get_dispute_tree(symbol).is_grounded
+        dispute_tree = aba.get_dispute_tree(symbol)
+        if dispute_tree is not None:
+            data['dispute_trees'][symbol] = jsonpickle.encode(json_graph.node_link_data(dispute_tree.graph), unpicklable=False, max_depth=6, make_refs=False)
+            data['dispute_trees_data'][symbol] = dict()
+            data['dispute_trees_data'][symbol]['is_conflict_free'] = aba.get_argument(symbol).is_conflict_free()
+            data['dispute_trees_data'][symbol]['is_admissible'] = dispute_tree.is_admissible
+            data['dispute_trees_data'][symbol]['is_grounded'] = dispute_tree.is_grounded
     
     return json.dumps(data)
 
