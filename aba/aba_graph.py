d214860fbfd98c77e77e759c927062b8dccd56f3
kenrick95 <kenrick95@gmail.com>
2016-08-10 11:53:42 +0800

    [WIP] big change in argument class to support "OR"

--- aba/aba_graph.py
+++ aba/aba_graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import logging
+import copy
 
 class ABA_Graph():
     """
@@ -8,77 +9,90 @@ class ABA_Graph():
     """
         
     def __init__(self, aba = None, root = None):
-        self.graph = nx.DiGraph() # Directed graph
+        self.graphs = []
+
+        self.__history = [[]]
+        self.__is_cyclical = [False]
+
+        self.assumptions = [{}]
+        self.is_conflict_free = [None]
+        self.is_stable = [None]
         
         self.root = root
         self.__aba = aba
+
+
+        self.graphs.append(nx.DiGraph())
+        self.__branches = 1
         
-        self.__history = []
-        self.__is_cyclical = False
+        self.graphs[0].add_node(root)
+        self.__history[0].append(root)
+        self.__propagate(0, root)
+        self.__history[0].pop()
 
-        self.graph.add_node(root)
-        self.__history.append(root)
-        self.__propagate(root)
-        self.__history.pop()
-
-        self.assumptions = {}
         self.__propagate_assumptions()
-
-        self.is_conflict_free = None
         self.__determine_is_conflict_free()
-
-        self.is_stable = None
         self.__determine_is_stable()
         
-    def __propagate(self, node):
-        # find rule in aba.rule to support node
-        for rule in self.__aba.rules:
-            if rule.result == node:
-                for symbol in rule.symbols:
-                    self.graph.add_edge(node, symbol)
-                    if symbol is not None:
-                        if symbol in self.__history:
-                            self.__is_cyclical = True
-                            break
-                        self.__history.append(symbol)
-                        self.__propagate(symbol)
-                        self.__history.pop()
-            if self.__is_cyclical:
-                break
-                    
+    def __propagate(self, index, node):
+        for i, rule in enumerate([x for x in self.__aba.rules if x.result == node]):
+            if i > 0: # "OR" branch, create new argument graph
+                self.graphs.append(self.graphs[index].copy())
+                self.__history.append(copy.deepcopy(self.__history[index]))
+                self.__is_cyclical.append(copy.deepcopy(self.__is_cyclical[index]))
+                self.assumptions.append({})
+                self.is_conflict_free.append(None)
+                self.is_stable.append(None)
+            
+            for symbol in rule.symbols:
+                self.graphs[index + i].add_edge(node, symbol)
+                if symbol is not None:
+                    if symbol in self.__history[index + i]:
+                        self.__is_cyclical[index + i] = True
+                        break
+                    self.__history[index + i].append(symbol)
+                    self.__propagate(index + i, symbol)
+                    self.__history[index + i].pop()
+ 
     def __propagate_assumptions(self):
         for assumption, symbol in self.__aba.contraries.items():
-            if assumption in self.graph.nodes():
-                # `assumption` is being attacked by `symbol`
-                self.assumptions[assumption] = symbol
+            for index, graph in enumerate(self.graphs):
+                if assumption in graph.nodes():
+                    # `assumption` is being attacked by `symbol`
+                    self.assumptions[index][assumption] = symbol
     
     def __determine_is_conflict_free(self):
-        conflict_free = True
-        for assumption, attacker in self.__aba.contraries.items():
-            if attacker in self.graph.nodes() and assumption in self.graph.nodes():
-                conflict_free = False
-                break
-        self.is_conflict_free = conflict_free
-        logging.debug("Argument <%s> is conflict free: %s", self.root, self.is_conflict_free)
+        for index, graph in enumerate(self.graphs):
+            conflict_free = True
+            for assumption, attacker in self.__aba.contraries.items():
+                if attacker in graph.nodes() and assumption in graph.nodes():
+                    conflict_free = False
+                    break
+            self.is_conflict_free[index] = conflict_free
+            logging.debug("Argument <%s> index <%d> is conflict free: %s", self.root, index, self.is_conflict_free)
 
     def __determine_is_stable(self):
-        stable = False
-        if self.is_conflict_free:
-            stable = True
-            for assumption, attacker in self.__aba.contraries.items():
-                if assumption not in self.graph.nodes():
-                    if attacker not in self.graph.nodes():
-                        stable = False
-                        break
-        
-        self.is_stable = stable
-        logging.debug("Argument <%s> is stable: %s", self.root, self.is_stable)
+        for index, graph in enumerate(self.graphs):
+            stable = False
+            if self.is_conflict_free:
+                stable = True
+                for assumption, attacker in self.__aba.contraries.items():
+                    if assumption not in graph.nodes():
+                        if attacker not in graph.nodes():
+                            stable = False
+                            break
+            
+            self.is_stable[index] = stable
+            logging.debug("Argument <%s> index <%d> is stable: %s", self.root, index, self.is_stable)
 
-    def __process_is_actual_argument(self, node):
-        if self.__is_cyclical:
+    def __process_is_actual_argument(self, index, node):
+        graph = self.graphs[index]
+        print(index, node, self)
+        if self.__is_cyclical[index]:
             return False
 
-        neighbors = self.graph.successors(node)
+        neighbors = graph.successors(node)
+        print(".. ", neighbors)
         if len(neighbors) == 0: # leaf node
             if node is None or node in self.__aba.assumptions:
                 return True
@@ -86,11 +100,11 @@ class ABA_Graph():
         
         ret = True
         for neighbor in neighbors:
-            ret = ret and self.__process_is_actual_argument(neighbor)
+            ret = ret and self.__process_is_actual_argument(index, neighbor)
         return ret
 
-    def is_actual_argument(self):
-        return self.__process_is_actual_argument(self.root)
+    def is_actual_argument(self, index = 0):
+        return self.__process_is_actual_argument(index, self.root)
 
     def __str__(self):
         return "Argument '%s'" % self.root
