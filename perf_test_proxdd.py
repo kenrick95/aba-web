280a4e7117f26af18222ef5364e6cce1c68cafd7
kenrick95 <kenrick95@gmail.com>
2016-09-13 22:35:25 +0800

    perf_test_proxdd: add timeout

--- perf_test_proxdd.py
+++ perf_test_proxdd.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 from aba.aba_perf_logger import ABA_Perf_Logger
 
 logging.basicConfig(filename=os.path.join('logs','ProxddPerfTest.log'),level=logging.DEBUG,format='[%(asctime)s] %(levelname)s:%(name)s: %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
@@ -10,6 +10,7 @@ logging.basicConfig(filename=os.path.join('logs','ProxddPerfTest.log'),level=log
 path_to_sictus = "C:\\Program Files (x86)\\SICStus Prolog VC14 4.3.3\\bin\\sicstus-4.3.3"
 path_to_proxdd = "D:/Cloud/SCE/FYP-proxdd/proxdd/code/proxdd.pl"
 path_to_exp_file = "D:\\Cloud\\SCE\\FYP-randomaf\\frameworks"
+timeout = 120
 
 runonly = []
 with open("perf_test_proxdd_setup.txt", "r") as f:
@@ -28,13 +29,19 @@ for x in runonly:
     perf_logger = ABA_Perf_Logger("%s, testing symbol %s" % (file, test_symbol))
     perf_logger.start()
 
-    p = Popen(path_to_sictus, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=path_to_exp_file)
+    stdout_data, stderr_data = None, None
+    proc = Popen(path_to_sictus, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=path_to_exp_file)
     cmd = "compile('%s').\nloadf(%s).\nsxdd(%s, X)." % (path_to_proxdd, file, test_symbol)
-    res = p.communicate(bytes(cmd, 'UTF-8'))
+    try:
+        stdout_data, stderr_data = proc.communicate(input=bytes(cmd, 'UTF-8'), timeout=timeout)
+    except TimeoutExpired:
+        proc.kill()
+        logging.error("Timeout error")
 
-    for line in res:
-        if line:
-            logging.debug(line.decode('UTF-8'))
+    if stdout_data:
+        logging.debug(stdout_data.decode('UTF-8'))
+    if stderr_data:
+        logging.error(stderr_data.decode('UTF-8'))
 
     perf_logger.end()
 
