9e225d33469267fbc5ea04eeca55882ebcdb45eb
kenrick95 <kenrick95@gmail.com>
2016-09-14 23:36:18 +0800

    use psutil to kill subprocess timeout

--- perf_test_proxdd.py
+++ perf_test_proxdd.py
@@ -4,6 +4,7 @@ import logging
 import os
 from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 from aba.aba_perf_logger import ABA_Perf_Logger
+import psutil
 
 logging.basicConfig(filename=os.path.join('logs','ProxddPerfTest.log'),level=logging.DEBUG,format='[%(asctime)s] %(levelname)s:%(name)s: %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -11,6 +12,13 @@ path_to_sictus = "C:\\Program Files (x86)\\SICStus Prolog VC14 4.3.3\\bin\\sicst
 path_to_proxdd = "D:/Cloud/SCE/FYP-proxdd/proxdd/code/proxdd.pl"
 path_to_exp_file = "D:\\Cloud\\SCE\\FYP-randomaf\\frameworks"
 timeout = 120
+
+# http://stackoverflow.com/a/25134985/917957
+def kill(proc_pid):
+    process = psutil.Process(proc_pid)
+    for sub_proc in process.children(recursive=True):
+        sub_proc.kill()
+    process.kill()
 
 runonly = []
 with open("perf_test_proxdd_setup.txt", "r") as f:
@@ -35,7 +43,7 @@ for x in runonly:
     try:
         stdout_data, stderr_data = proc.communicate(input=bytes(cmd, 'UTF-8'), timeout=timeout)
     except TimeoutExpired:
-        proc.kill()
+        kill(proc.pid)
         logging.error("Timeout error")
 
     if stdout_data:
